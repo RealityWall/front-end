9b79d812b613c537c3f676ede187a57af41d7c04
Jean-Yves DELMOTTE <delmotte.jy@gmail.com>
2015-11-11 22:19:57 +0100

    front end continuation,

--- js/components/wall/wall.controller.js
+++ js/components/wall/wall.controller.js
@@ -15,6 +15,15 @@
         $interval(function () {
             vm.seconds = (vm.seconds == 0 ) ? 59 : vm.seconds -1;
         }, 1000);
+
+        vm.posts = [
+            { rank: 1, title: 'Vive la liberté iufhi uerhfiu efieu riuheriufeiueiufieu iuerierifue  !' },
+            { rank: 3, title: 'Bonjour la vie' },
+            { rank: 2, title: 'cc les gens' },
+            { rank: 4, title: 'cc les gens' },
+            { rank: 5, title: 'cc les gens' },
+            { rank: 6, title: 'cc les gens' }
+        ];
     }
 
 })();
