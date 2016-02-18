52e3a4f0261f740225d47b11fc01b148b2e7801f
Jean-Yves Delmotte <jean-yves.delmotte@greencom-networks.com>
2016-02-18 22:44:39 +0100

    modify logo when logged on small screens

--- src/js/components/App/Header.jsx
+++ src/js/components/App/Header.jsx
@@ -9,7 +9,7 @@ var Header = React.createClass({
         console.log(this.props.user);
         return (
             <div id="header">
-                <div className="header-content">
+                <div className={"header-content " + (this.props.user.id ? "logged":"")}>
                     <a href="/"><img src="img/full-logo.png" alt="main logo"/></a>
                     <a href="/"><img src="img/logo.png" alt="main logo"/></a>
                     { this.props.user.id ? null : <a href="/sign-in" className="btn" id="sign-in">Inscription</a> }
